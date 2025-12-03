a36345973a1954270aef305f46b195de1f595353
GhostWalker562 <43276017+GhostWalker562@users.noreply.github.com>
2025-12-03 11:16:12 -0800

    Update React and Next.js

--- apps/docs/app/docs/ts-aptos-labs-wallet-standard/_meta.tsx
+++ apps/docs/app/docs/ts-aptos-labs-wallet-standard/_meta.tsx
@@ -2,7 +2,4 @@ import type { MetaRecord } from "nextra";
 
 export default {
   "quick-start": "Quick Start",
-  "aptos-sign-in": "aptos:signIn",
-  "aptos-sign-in-input": "AptosSignInInput",
-  "aptos-sign-in-output": "AptosSignInOutput",
 } satisfies MetaRecord;
